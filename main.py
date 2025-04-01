49b8c25410531988b7a3cac8c0177a83bd86eae6
rftbrkzkn <burakozkan910@gmail.com>
2025-04-01 22:46:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from hesaplama.toplama import topla
 from hesaplama.cikarma import cikar
-
+from hesaplama.carpma import carp
+from hesaplama.bolme import bolme
 def main():
     a = 10
     b = 5
     print(f"Toplam: {topla(a, b)}")
     print(f"Fark: {cikar(a, b)}")
+    print(f"Carpim: {carp(a,b)}")
+    print(f"Bolum: {bolme(a,b)}")
 
 if __name__ == "__main__":
     main()
